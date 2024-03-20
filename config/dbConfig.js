c2ac3e76944310b28ef7ad73132474711aed6192
Vikash <rautelavikash27@gmail.com>
2024-03-21 00:14:54 +0530

    Set Environment Variables

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'tuf'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_DATABASE || 'tuf'
 };
 
 const connection = mysql.createConnection(dbConfig);
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 // backend/app.js
 const express = require('express');
 const cors = require('cors')
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 var morgan = require('morgan') ///to be removed
+dotenv.config();
+
 const submissionRoutes = require('./routes/submissionRoutes');
 
 const app = express();
